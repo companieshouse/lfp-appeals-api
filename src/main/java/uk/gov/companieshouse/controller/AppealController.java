8d9c60b4963f3b54f3e155d13a2c89e1967ea14c
Natalie Blanchette <natalieb@kainos.com@Cano.local>
2020-01-22 16:43:38 +0000

    Set up of spring boot application

---
+++ src/main/java/uk/gov/companieshouse/controller/AppealController.java
@@ -0,0 +1,8 @@
+package uk.gov.companieshouse.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppealController {
+
+}
---
+++ src/main/java/uk/gov/companieshouse/service/AppealService.java
@@ -0,0 +1,8 @@
+package uk.gov.companieshouse.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppealService {
+
+}
