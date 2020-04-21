6e2a14542466c947452fb0a977f8f7856f37260c
Damian Dunajski <damdun@gmail.com>
2020-04-21 11:54:39 +0100

    Lets break it

--- src/main/java/uk/gov/companieshouse/AppealApplication.java
+++ src/main/java/uk/gov/companieshouse/AppealApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppealApplication {
+public class AppealApplication1 {
 
     public static void main (String[] args) {
         SpringApplication.run(AppealApplication.class, args);
