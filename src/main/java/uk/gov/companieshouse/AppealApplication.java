3f172c9c4a59c240937d717147d23b559e544d72
Damian Dunajski <damdun@gmail.com>
2020-04-21 12:16:47 +0100

    Fix with sonar issue

--- src/main/java/uk/gov/companieshouse/AppealApplication.java
+++ src/main/java/uk/gov/companieshouse/AppealApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppealApplication1 {
+public class AppealApplication {
 
     public static void main (String[] args) {
+        for (int i = 0; i < 4; i++) {
+            System.out.println("i = " + i);
+        }
         SpringApplication.run(AppealApplication.class, args);
     }
 }
