92d11f517202f08b07deff729e5092935cf7eab8
Juliano Saunders <me@jsaunders.dev>
2020-04-24 15:38:48 +0100

    feat: support legacy penalty reference numbers

--- src/main/java/uk/gov/companieshouse/model/PenaltyIdentifier.java
+++ src/main/java/uk/gov/companieshouse/model/PenaltyIdentifier.java
@@ -7,12 +7,12 @@ import java.util.Objects;
 public class PenaltyIdentifier {
 
     @NotBlank(message = "companyNumber must not be blank")
-    @Pattern(regexp = "(((SC|NI)[0-9]{1,6})|([0-9]{1,8}))",
+    @Pattern(regexp = "((SC|NI)[0-9]{1,6}|[0-9]{1,8})",
         message = "companyNumber is invalid")
     private String companyNumber;
 
     @NotBlank(message = "penaltyReference must not be blank")
-    @Pattern(regexp = "([A-Z]{1}[0-9]{8})",
+    @Pattern(regexp = "([A-Z][0-9]{8})|(PEN[1-9]A/((SC|NI)[0-9]{1,6}|[0-9]{1,8}))",
         message = "penaltyReference is invalid")
     private String penaltyReference;
 
