11b3c6a514d939e16147288df1ef0907352125b5
Francisco <fgoinhas@companieshouse.gov.uk>
2020-06-24 15:12:30 +0100

    refactor: changed to jupiter assertion

--- src/test/java/uk/gov/companieshouse/model/validation/CompanyNumberValidatorTest.java
+++ src/test/java/uk/gov/companieshouse/model/validation/CompanyNumberValidatorTest.java
@@ -1,7 +1,7 @@
 package uk.gov.companieshouse.model.validation;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
