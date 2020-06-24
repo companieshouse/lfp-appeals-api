1b5932d1bf8f885f054005370cfe5f4b2717e40a
Francisco <fgoinhas@companieshouse.gov.uk>
2020-06-24 15:43:28 +0100

    fix: renamed test validator package

--- src/test/java/uk/gov/companieshouse/model/validator/CompanyNumberValidatorTest.java
+++ src/test/java/uk/gov/companieshouse/model/validator/CompanyNumberValidatorTest.java
@@ -1,10 +1,11 @@
-package uk.gov.companieshouse.model.validation;
+package uk.gov.companieshouse.model.validator;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,8 +13,6 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import uk.gov.companieshouse.model.validator.CompanyNumberValidator;
 
 @ExtendWith(SpringExtension.class)
 public class CompanyNumberValidatorTest {
@@ -23,18 +22,15 @@ public class CompanyNumberValidatorTest {
 
         List<String> invalidLists = List.of(",", "A,[.", "A.BC.D");
 
-        invalidLists.forEach(prefixList -> {
-            assertThrows(IllegalArgumentException.class, () -> new CompanyNumberValidator(prefixList), prefixList);
-        });
+        invalidLists.forEach(prefixList -> assertThrows(IllegalArgumentException.class,
+                () -> new CompanyNumberValidator(prefixList), prefixList));
 
     }
 
     @Test
     void shouldCreateFactorySuccessfulyWhenValidListsProvided() {
         List<String> validLists = List.of("NI", "AB,CD,EF", "A,B,C,D,F", "AB,cd,e,F");
-        validLists.forEach(prefixList -> {
-            assertDoesNotThrow(() -> new CompanyNumberValidator(prefixList), prefixList);
-        });
+        validLists.forEach(prefixList -> assertDoesNotThrow(() -> new CompanyNumberValidator(prefixList), prefixList));
     }
 
     @Test
@@ -45,8 +41,8 @@ public class CompanyNumberValidatorTest {
 
         List<String> invalidCompanyNumbers = List.of("TY000001", "PTABC0123", "XC999999", "T0000001");
 
-        boolean isAllInvalid = invalidCompanyNumbers.stream().map(value -> companyNumberValidator.isValid(value, null))
-                .allMatch(result -> result == false);
+        boolean isAllInvalid = invalidCompanyNumbers.stream()
+                .noneMatch(value -> companyNumberValidator.isValid(value, null));
 
         assertTrue(isAllInvalid);
 
@@ -61,12 +57,11 @@ public class CompanyNumberValidatorTest {
         List<String> validUpperCaseCompanyNumbers = List.of("NI000000", "SC123123", "OC123123", "SO123123", "R0000000",
                 "R123", "123", "AP123456");
 
-        List<String> validLowerCaseCompanyNumbers = validUpperCaseCompanyNumbers.stream()
-                .map(companyNumber -> companyNumber.toLowerCase()).collect(Collectors.toList());
+        List<String> validLowerCaseCompanyNumbers = validUpperCaseCompanyNumbers.stream().map(String::toLowerCase)
+                .collect(Collectors.toList());
 
         boolean isAllValid = Stream.of(validUpperCaseCompanyNumbers, validLowerCaseCompanyNumbers)
-                .flatMap(x -> x.stream()).map(value -> companyNumberValidator.isValid(value, null))
-                .allMatch(result -> result == true);
+                .flatMap(Collection::stream).allMatch(value -> companyNumberValidator.isValid(value, null));
 
         assertTrue(isAllValid);
 
