d84f19a51ddff5b5aca63404f5ca7ca299ef0e09
mreis-ch <mreis@companieshouse.gov.uk>
2021-10-11 16:45:25 +0100

    Updated email content to match what is sent from the front-end
    BI-8916

--- src/main/java/uk/gov/companieshouse/service/EmailService.java
+++ src/main/java/uk/gov/companieshouse/service/EmailService.java
@@ -1,6 +1,7 @@
 package uk.gov.companieshouse.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
@@ -10,7 +11,6 @@ import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import uk.gov.companieshouse.AppealApplication;
 import uk.gov.companieshouse.email.EmailSend;
@@ -19,7 +19,9 @@ import uk.gov.companieshouse.kafka.exceptions.SerializationException;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 import uk.gov.companieshouse.model.Appeal;
+import uk.gov.companieshouse.model.Attachment;
 import uk.gov.companieshouse.model.PenaltyIdentifier;
+import uk.gov.companieshouse.model.Reason;
 
 /**
  * Communicates with <code>chs-email-sender</code> via the <code>send-email</code> Kafka topic to
@@ -31,38 +33,21 @@ public class EmailService {
     private static final Logger LOGGER = LoggerFactory.getLogger(AppealApplication.APP_NAMESPACE);
 
     private static final String LFP_APPEALS_API_APP_ID =
-    		AppealApplication.APP_NAMESPACE;
+    		"lfp-appeals-api";
     private static final String LFP_APPEAL_SUBMISSION_INTERNAL_MESSAGE_TYPE =
             "lfp-appeal-submission-internal";
     private static final String LFP_APPEAL_SUBMISSION_CONFIRMATION_MESSAGE_TYPE =
             "lfp-appeal-submission-confirmation";
-    private static final String ITEM_TYPE_CERTIFICATE = "certificate";
-    private static final String ITEM_TYPE_CERTIFIED_COPY = "certified-copy";
-    private static final String ITEM_TYPE_MISSING_IMAGE_DELIVERY = "missing-image-delivery";
 
     /**
      * This email address is supplied only to satisfy Avro contract.
      */
-    private static final String TOKEN_EMAIL_ADDRESS = "chs-orders@ch.gov.uk";
+    private static final String TOKEN_EMAIL_ADDRESS = "lfp-appeals@ch.gov.uk";
 
-//    private final OrderDataToCertificateOrderConfirmationMapper orderToCertificateOrderConfirmationMapper;
-//    private final OrderDataToItemOrderConfirmationMapper orderToItemOrderConfirmationMapper;
-    private final ObjectMapper objectMapper;
-    private final EmailSendMessageProducer producer;
+     private final EmailSendMessageProducer producer;
 
-//    @Value("${certificate.order.confirmation.recipient}")
-//    private String certificateOrderRecipient;
-//    @Value("${certified-copy.order.confirmation.recipient}")
-//    private String certifiedCopyOrderRecipient;
 
-    public EmailService(
-//            final OrderDataToCertificateOrderConfirmationMapper orderToConfirmationMapper,
-//            final OrderDataToItemOrderConfirmationMapper orderToItemOrderConfirmationMapper,
-            final ObjectMapper objectMapper,
-            final EmailSendMessageProducer producer) {
-//        this.orderToCertificateOrderConfirmationMapper = orderToConfirmationMapper;
-//        this.orderToItemOrderConfirmationMapper = orderToItemOrderConfirmationMapper;
-        this.objectMapper = objectMapper;
+    public EmailService(EmailSendMessageProducer producer) {
         this.producer = producer;
     }
 
@@ -84,6 +69,7 @@ public class EmailService {
 
     /**
      * Sends out a certificate or certified copy order confirmation email.
+     * @param messageType 
      *
      * @param appeal the order information used to compose the order confirmation email.
      * @throws JsonProcessingException
@@ -91,22 +77,37 @@ public class EmailService {
      * @throws ExecutionException
      * @throws SerializationException
      */
+    private EmailSend getEmailHeader(String messageType) {
+
+        EmailSend email = new EmailSend();
+
+        email.setAppId(LFP_APPEALS_API_APP_ID);
+        email.setMessageType(messageType);
+        email.setEmailAddress(TOKEN_EMAIL_ADDRESS);
+        email.setMessageId(UUID.randomUUID().toString());
+        email.setCreatedAt(LocalDateTime.now().toString());
+        
+        return email;
+    }
+
     public void sendInternalConfirmationEmail(final Appeal appeal)
     		throws SerializationException, ExecutionException, InterruptedException {
 
-        final EmailSend email = new EmailSend();
+        final EmailSend email = getEmailHeader(LFP_APPEAL_SUBMISSION_INTERNAL_MESSAGE_TYPE);
 
-        email.setAppId(LFP_APPEALS_API_APP_ID);
-        email.setMessageType(LFP_APPEAL_SUBMISSION_INTERNAL_MESSAGE_TYPE);
-        email.setEmailAddress(TOKEN_EMAIL_ADDRESS);
-        email.setMessageId(UUID.randomUUID().toString());
-        
-        //email.setData(objectMapper.writeValueAsString(confirmation));
-        email.setCreatedAt(LocalDateTime.now().toString());
+        email.setData(buildEmailContent(appeal));
 
-        String orderReference = appeal.getPenaltyIdentifier().getPenaltyReference();
-        //LoggingUtils.logWithOrderReference("Sending confirmation email for order", orderReference);
-        producer.sendMessage(email, orderReference);
+        producer.sendMessage(email, appeal.getPenaltyIdentifier().getPenaltyReference());
+    }
+
+    public void sendExternalConfirmationEmail(final Appeal appeal)
+    		throws SerializationException, ExecutionException, InterruptedException {
+
+        final EmailSend email = getEmailHeader(LFP_APPEAL_SUBMISSION_CONFIRMATION_MESSAGE_TYPE);
+
+        email.setData(buildEmailContent(appeal));
+
+        producer.sendMessage(email, appeal.getPenaltyIdentifier().getPenaltyReference());
     }
 
     /**
@@ -119,77 +120,87 @@ public class EmailService {
      * @throws InterruptedException
      * @throws ExecutionException
      * @throws SerializationException
-     */
-//    public void sendExternalConfirmationEmail(final Appeal appeal) {
-//
-//        final EmailSend email = new EmailSend();
-//
-//        email.setAppId(LFP_APPEALS_API_APP_ID);
-//        email.setMessageType(LFP_APPEAL_SUBMISSION_CONFIRMATION_MESSAGE_TYPE);
-//        email.setEmailAddress(TOKEN_EMAIL_ADDRESS);
-//        email.setMessageId(UUID.randomUUID().toString());
-//        
-//        email.setData(objectMapper.writeValueAsString(confirmation));
-//        email.setCreatedAt(LocalDateTime.now().toString());
-//
-//        String orderReference = appeal.getPenaltyIdentifier().getPenaltyReference();
-//        //LoggingUtils.logWithOrderReference("Sending confirmation email for order", orderReference);
-//        producer.sendMessage(email, orderReference);
-//    }
+     */    
+	private String buildEmailContent(Appeal appeal) {
 
-//    private void buildData(Appeal appeal) {
-//        Map<String, Object> data = new HashMap<>();
-//
-//        LocalDateTime submittedOn = appeal.getCreatedAt();
-//
-//        data.put("subject", "Appeal submitted - " + appeal.getPenaltyIdentifier().getCompanyNumber());
-//        data.put("objection_id", objection.getId());
-//        data.put("to", TOKEN_EMAIL_ADDRESS);
-//        data.put("full_name", objection.getCreatedBy().getFullName());
-//        data.put("share_identity", objection.getCreatedBy().isShareIdentity());
-//        data.put("company_name", companyName);
-//        data.put("company_number", objection.getCompanyNumber());
-//        data.put("reason", objection.getReason());
-//        data.put("attachments", objection.getAttachments());
-//        data.put("attachments_download_url_prefix", emailAttachmentDownloadUrlPrefix);
-//    }
-    
-	private void buildJson(Appeal appeal) throws JSONException {
-
+		String email = appeal.getCreatedBy().getEmailAddress();
 		PenaltyIdentifier penaltyIdentifier = appeal.getPenaltyIdentifier();
 		String companyNumber = penaltyIdentifier.getCompanyNumber();
 
-		JSONArray jsonArray = new JSONArray();
-		
-		JSONObject jsonHead = new JSONObject();
-
-		jsonHead.put("to", TOKEN_EMAIL_ADDRESS);
-		jsonHead.put("subject", "Appeal submitted - " + companyNumber);
-		jsonArray.put(jsonHead);
+		JSONObject jsonEmailContent = new JSONObject();
+		jsonEmailContent.put("to", email);
+		jsonEmailContent.put("subject", "Appeal submitted - " + companyNumber);
 		
 		JSONObject jsonBody = new JSONObject();
+    	jsonBody.put("templateName", LFP_APPEAL_SUBMISSION_INTERNAL_MESSAGE_TYPE);
 
-    	jsonBody.put("templateName", "lfp-appeal-submission-internal");
-        //data.put("templateData", );
-        JSONObject companyDetails = new JSONObject();
-        //companyDetails.put("companyName", getCompanyName(companyNumber));
-        companyDetails.put("companyNumber", companyNumber);
-        companyDetails.put("penaltyReference", penaltyIdentifier.getPenaltyReference());
-        jsonArray.put(companyDetails);
-        JSONObject userProfile = new JSONObject();
-        userProfile.put("email", appeal.getCreatedBy().getEmailAddress());
-        jsonArray.put(userProfile);
-//        data.put("userProfile: {
-//                email: userProfile.email
-//            },
-//            reasons: buildEmailReasonContent(appeal)
-//    	
-//		json.t
+        JSONObject jsonTemplateData = new JSONObject();
+        jsonTemplateData.put("companyName", "company name");//getCompanyName(companyNumber));
+        jsonTemplateData.put("companyNumber", companyNumber);
+        jsonTemplateData.put("penaltyReference", penaltyIdentifier.getPenaltyReference());
+
+        JSONObject jsonUserProfile = new JSONObject();
+        jsonUserProfile.put("email", email);
+        jsonTemplateData.put("userProfile", jsonUserProfile);
+        jsonTemplateData.put("reasons", addReasonsData(appeal));
+
+        jsonBody.put("templateData", jsonTemplateData);
+        jsonEmailContent.put("body", jsonBody);
         
-        LOGGER.info("JSON Array: " + jsonArray.toString());
+        LOGGER.info("JSON Array: " + jsonEmailContent.toString());
+        
+        return jsonEmailContent.toString();
+	}
+	
+	private JSONObject addReasonsData(Appeal appeal) {
+
+		Reason appealReason = appeal.getReason();
+		
+		JSONObject jsonReason = new JSONObject();
+		jsonReason.put("name", appeal.getCreatedBy().getName());
+		if( appealReason.getOther() != null ) {
+			jsonReason.put("relationshipToCompany", appeal.getCreatedBy().getRelationshipToCompany());
+			jsonReason.put("title", appeal.getReason().getOther().getTitle());
+			jsonReason.put("description", appeal.getReason().getOther().getDescription());
+		} else {
+			jsonReason.put("illPerson", appeal.getReason().getIllness().getIllPerson());
+			jsonReason.put("illnessStart", appeal.getReason().getIllness().getIllnessStart());
+			jsonReason.put("illnessEnd", appeal.getReason().getIllness().getIllnessEnd());		
+		}
+		jsonReason.put("description", appeal.getReason().getOther().getDescription());
+		jsonReason.put("attachments", addAttachments(appeal));
+        
+		JSONObject fullReason = new JSONObject();
+		if( appealReason.getOther() != null ) {
+			fullReason.put("other", jsonReason);
+		} else {
+			fullReason.put("illness", jsonReason);
+		}
+        return fullReason;
 		
 	}
     
+	private JSONArray addAttachments(Appeal appeal) {
+
+		Reason reason = appeal.getReason();
+
+		JSONArray attachmentArray = new JSONArray();
+
+		List<Attachment> attachments;
+		if( reason.getOther() != null ) {
+			attachments = reason.getOther().getAttachments();
+		} else {
+			attachments = reason.getIllness().getAttachments();
+		}
+		attachments.stream().forEach(a -> {
+			JSONObject jsonAttachments = new JSONObject();
+			jsonAttachments.put("name", a.getName());
+			jsonAttachments.put("url", a.getUrl() + "&a=" + appeal.getId());
+			attachmentArray.put(jsonAttachments);
+		});
+		
+		return attachmentArray;
+	}
 //	private String getCompanyName(String companyNumber)
 //		throws ServiceException {
 //        try {
@@ -218,42 +229,4 @@ public class EmailService {
 //                    e);
 //        }
 //    }
-
-    /**
-     * Builds the order confirmation and email based on the order provided.
-     * @param order the order for which an email confirmation is to be sent
-     * @return a {@link OrderConfirmationAndEmail} holding both the confirmation and its email envelope
-     */
-//    private OrderConfirmationAndEmail buildOrderConfirmationAndEmail(final OrderData order) {
-//        final String descriptionId = order.getItems().get(0).getDescriptionIdentifier();
-//        final EmailSend email = new EmailSend();
-//        final OrderConfirmation confirmation;
-//        switch (descriptionId) {
-//            case ITEM_TYPE_CERTIFICATE:
-//                confirmation = orderToCertificateOrderConfirmationMapper.orderToConfirmation(order);
-//                confirmation.setTo(certificateOrderRecipient);
-//                email.setAppId(LFP_APPEALS_API_APP_ID);
-//                email.setMessageType(LFP_APPEAL_SUBMISSION_INTERNAL_MESSAGE_TYPE);
-//                return new OrderConfirmationAndEmail(confirmation, email);
-//            case ITEM_TYPE_CERTIFIED_COPY:
-//                confirmation = orderToItemOrderConfirmationMapper.orderToConfirmation(order);
-//                confirmation.setTo(certifiedCopyOrderRecipient);
-//                email.setAppId(CERTIFIED_COPY_ORDER_NOTIFICATION_API_APP_ID);
-//                email.setMessageType(CERTIFIED_COPY_ORDER_NOTIFICATION_API_MESSAGE_TYPE);
-//                return new OrderConfirmationAndEmail(confirmation, email);
-//            case ITEM_TYPE_MISSING_IMAGE_DELIVERY:
-//                confirmation = orderToItemOrderConfirmationMapper.orderToConfirmation(order);
-//                confirmation.setTo(missingImageDeliveryOrderRecipient);
-//                email.setAppId(MISSING_IMAGE_DELIVERY_ORDER_NOTIFICATION_API_APP_ID);
-//                email.setMessageType(MISSING_IMAGE_DELIVERY_ORDER_NOTIFICATION_API_MESSAGE_TYPE);
-//                return new OrderConfirmationAndEmail(confirmation, email);
-//            default:
-//                final Map<String, Object> logMap = LoggingUtils.createLogMapWithOrderReference(order.getReference());
-//                final String error = "Unable to determine order confirmation type from description ID " +
-//                        descriptionId + "!";
-//                LOGGER.error(error, logMap);
-//                throw new ServiceException(error);
-//        }
-//    }
-
 }
