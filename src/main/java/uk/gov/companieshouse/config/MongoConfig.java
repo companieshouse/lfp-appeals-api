79767bfc7410f09988e7edd3a1d339a8c6fabf3a
adr1ancho <adrian.cho.j@gmail.com>
2022-12-05 13:09:53 +0000

    Address code smell

--- src/main/java/uk/gov/companieshouse/config/MongoConfig.java
+++ src/main/java/uk/gov/companieshouse/config/MongoConfig.java
@@ -5,14 +5,17 @@ import com.mongodb.TransactionOptions;
 import com.mongodb.WriteConcern;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 
 public class MongoConfig {
 
     @Bean
     MongoTransactionManager mongoTransactionManager(MongoDatabaseFactory dbFactory) {
-        TransactionOptions transactionOptions = TransactionOptions.builder().readConcern(ReadConcern.LOCAL).writeConcern(WriteConcern.W1).build();
+        var transactionOptions = TransactionOptions
+            .builder()
+            .readConcern(ReadConcern.LOCAL)
+            .writeConcern(WriteConcern.W1)
+            .build();
         return new MongoTransactionManager(dbFactory, transactionOptions);
     }
 }
