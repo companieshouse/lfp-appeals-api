1a632c6377e01f904cafe07eb9ed3929db683a32
tmoller-ch <tmoller@companieshouse.gov.uk>
2024-06-11 13:31:55 +0100

    update healthcheck path

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -11,7 +11,7 @@ locals {
   kms_alias                  = "alias/${var.aws_profile}/environment-services-kms"
   lb_listener_rule_priority  = 93
   lb_listener_paths          = ["/companies/*/appeals","/api-docs/lfp-appeals-api"]
-  healthcheck_path           = "" #healthcheck path for lfp appeals api
+  healthcheck_path           = "/lfp-appeals-api/healthcheck" #healthcheck path for lfp appeals api
   healthcheck_matcher        = "200"
   s3_config_bucket           = data.vault_generic_secret.shared_s3.data["config_bucket_name"]
   app_environment_filename   = "lfp-appeals-api.env"
