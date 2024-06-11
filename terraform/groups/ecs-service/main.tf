f380b27ad4dbe85cabf5a3ea49a79c1217ab32bc
tmoller-ch <tmoller@companieshouse.gov.uk>
2024-06-11 09:11:22 +0100

    update var

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -62,7 +62,7 @@ module "ecs-service" {
   # Docker container details
   docker_registry   = var.docker_registry
   docker_repo       = local.docker_repo
-  container_version = var.company_accounts_api_version
+  container_version = var.lfp_appeals_api_version
   container_port    = local.container_port
 
   # Service configuration
