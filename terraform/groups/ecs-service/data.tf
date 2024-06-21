3f8969d968fdb10acd010c887669b83541d682af
tmoller-ch <163420190+tmoller-ch@users.noreply.github.com>
2024-06-21 10:50:57 +0100

    update lb (#226)

--- terraform/groups/ecs-service/data.tf
+++ terraform/groups/ecs-service/data.tf
@@ -34,21 +34,12 @@ data "aws_iam_role" "ecs_cluster_iam_role" {
 }
 
 data "aws_lb" "service_lb" {
-  name = "${var.environment}-chs-apichgovuk"
+  name = "${var.environment}-chs-internalapi"
 }
 
 data "aws_lb_listener" "service_lb_listener" {
   load_balancer_arn = data.aws_lb.service_lb.arn
   port              = 443
-}
-
-data "aws_lb" "secondary_lb" {
-  name = "${var.environment}-chs-apichgovuk-private"
-}
-
-data "aws_lb_listener" "secondary_lb_listener" {
-  load_balancer_arn = data.aws_lb.secondary_lb.arn
-  port = 443
 }
 
 # retrieve all secrets for this stack using the stack path
